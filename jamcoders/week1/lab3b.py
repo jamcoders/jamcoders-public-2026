6bd89bf107918ce95361d45463e818d2b5399d4b
ftshang <112454332+ftshang@users.noreply.github.com>
2026-06-29 19:37:38 -0700

    Update lab3b.py

--- jamcoders/week1/lab3b.py
+++ jamcoders/week1/lab3b.py
@@ -11,5 +11,14 @@ def create_check_answer(correct, num):
         check_answers_with_num(ans, correct, num)
     return check_fn
 
+# From 2023 Labs
+check_answer_2_1a = create_check_answer(10, '2.1a')
+check_answer_2_1b = create_check_answer(14, '2.1b')
+check_answer_2_1c_1 = create_check_answer(False, '2.1c_1')
+check_answer_2_1c_2 = create_check_answer(14, '2.1c_2')
+check_answer_2_1d_1 = create_check_answer(True, '2.1d_1')
+check_answer_2_1d_2 = create_check_answer(13, '2.1d_2')
+
+# From 2025 Labs
 check_answer_0_1 = create_check_answer('Hello JamCoders', '0.1')
 check_answer_0_4 = create_check_answer('JamCoders', '0.4')
