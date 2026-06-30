dcdd5ff53e1d6319996a17b24f14c0f4a7adbe49
ftshang <112454332+ftshang@users.noreply.github.com>
2026-06-29 19:21:21 -0700

    Update lab2a.py

--- jamcoders/week1/lab2a.py
+++ jamcoders/week1/lab2a.py
@@ -5,6 +5,33 @@ def check_answers(answer, correct, num):
     else:
         print(f"Your answer to Question {num}: '{answer}' is wrong :( try again!")
 
+# From 2023 Labs
+
+def answer_true(ans,num):
+    answer = True
+    check_answers(ans, answer,num)
+
+def answer_false(ans,num):
+    answer = False
+    check_answers(ans, answer, num)
+
+# Option 1: Trying it for now
+def check_answer1(ans): answer_false(ans,1)
+def check_answer2(ans): answer_false(ans,2)
+def check_answer3(ans): answer_true(ans,3)
+def check_answer4(ans): answer_true(ans,4)
+def check_answer5(ans): answer_true(ans,5)
+def check_answer6(ans): answer_false(ans,6)
+def check_answer7(ans): answer_true(ans,7)
+def check_answer8(ans): answer_true(ans,8)
+
+# Trying this
+def check_answer_3_4(ans):
+    answers = [False,True,False,False,True,True,True]
+    for i in range(len(answers)):
+        check_answers(ans[i],answers[i], i+1)
+
+# From 2025 Labs
 
 def check_answer_0_1(answer):
     check_answers(answer, False, "0.1")
